6b98bc032873448eb0a06e1c04abb56261bab0af
diilfadhil <diifadhil21@gmail.com>
2026-08-06 08:56:07 +0700

    Tugas 10

---
+++ ppkd_belajar_b3/ppkd_belajar/lib/tugas_10/result_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class ResultPage extends StatelessWidget {
+  final String nama;
+  final String email;
+  final String hp;
+
+  const ResultPage({
+    super.key,
+    required this.nama,
+    required this.email,
+    required this.hp,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Hasil Pendaftaran")),
+
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              Lottie.asset("assets/animation/ceklis.json", height: 220),
+
+              const SizedBox(height: 20),
+
+              Text(
+                "Terima kasih,\n$nama telah mendaftar diaplikasi kami.",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Text("Nama : $nama"),
+                      Text("Email : $email"),
+                      Text("No HP : $hp"),
+                    ],
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Kembali"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
